acd90c8b0298428b2d76d5cf4c28ea4831536dbc
Caloric <100364040+RutiUs@users.noreply.github.com>
2023-06-08 19:12:22 +0300

    main.js

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ $(document).ready(function () {
         dots: true,
         slidesToShow: 1,
         slidesToScroll: 1,
-        nextArrow: '<button type="button" class="slick-arrow slick-prev"><img src="images/ikon/left-arrow.svg"></button>',
-        prevArrow: '<button type="button" class="slick-arrow slick-next"><img src="images/ikon/right-arrow.svg"</button>',
+        nextArrow: '<button type="button" class="slick-arrow slick-prev"><img src="../images/ikon/left-arrow.svg"></button>',
+        prevArrow: '<button type="button" class="slick-arrow slick-next"><img src="../images/ikon/right-arrow.svg"</button>',
         responsive: [
           {
             breakpoint: 1024,
